89630d2c9b3528cb2330b9d230e5c46b13b8f7ef
Bellarivetti <isabellarivetti@gmail.com>
2016-10-07 11:20:00 -0700

    Updated Files

---
+++ APLesson_4/Subwoofer.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Subwoofer
+{
+
+   public static void main(String[]args) 
+   {
+      Scanner input = new Scanner(System.in);
+	  Subwoofer boom = new Subwoofer();
+
+      double h = 0;
+      double l = 0;
+      double w = 0;
+
+      double volume = 0;
+  
+      System.out.print("Enter the height: "); 
+      height = input.nextDouble();
+
+      System.out.print("Enter the length: "); 
+      length = input.nextDouble();
+
+      System.out.print("Enter the width: "); 
+      width = input.nextDouble();
+
+      volume = h * l * w; 
+	 double volume1 = boom.volume1(volume);
+  
+      System.out.println("");
+      System.out.println("The volume is  "+ volume+ " cubic inches");
+	  System.out.println("The volume in cubic feet is "+ volume1);
+	
+   }
+	public double volume1 (double volume){	
+	return(volume * 0.00057870);
+	}
+}
