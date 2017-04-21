6529a55c84966dcac919dc8e7f89869b12b6a4fa
Bellarivetti <isabellarivetti@gmail.com>
2017-04-21 11:08:38 -0700

    MAGPIE LABS EVERYTHING
    yuh

---
+++ APCase_Magpie/Magpie1.java
@@ -0,0 +1,153 @@
+public class Magpie2
+
+{
+	/** Get a default greeting @return a greeting*/
+	public String getGreeting()
+	{
+		return "Hello... let's talk.";
+	}
+	
+	/** getResponse() method
+	 * ===========================================
+	 * 	Gives a response to a user statement
+	 *  @param statement (the user statement)
+	 * 	@return a response based on the rules given */
+	public String getResponse(String statement)
+	
+	{
+		String response = "";
+		
+		if (statement.length() == 0)			
+		{
+			System.out.println("Say something, please.");
+		}
+
+		/** Exercise_01:
+		 * ==================================================
+		 * 	Code that asks the user "Say something, please."
+		 * 	if you enter nothing, or if you accidentally hit
+		 * 	enter. Think to yourself: "What is the length of
+		 * 	an empty String?" */
+
+
+		/** To be completed in Exercise_02:
+		 * 	Modify the following code to use the findKeyword
+		 * 	Method (details in "Exercise_02" below. */
+		else if (findKeyword(statement, "No.") >= 0)			
+		{
+			response = "Why are you so negative?";
+		}
+	
+
+		else if (findKeyword(statement, "Mother") >= 0
+				|| findKeyword(statement, "Father") >= 0
+				|| findKeyword(statement, "Sister") >= 0
+				|| findKeyword(statement, "Brother") >= 0)
+		
+		{
+			response = "Tell me more about your family.";
+		}
+		
+		else if (findKeyword(statement, "Cat") >= 0
+				|| findKeyword(statement, "Dog") >= 0
+				|| findKeyword(statement, "Fish") >= 0
+				|| findKeyword(statement, "Horse") >= 0)
+		
+		{
+			response = "Tell me more about your pet.";	
+		}
+		
+		else if (findKeyword(statement, "Robinette") >= 0)			
+		{
+			response = "He sounds like a pretty wannabe dank teacher.";	
+		}
+		/** Exercise_03(Final)
+		 * ==================================================
+		 * Create additional code (another else if) that
+		 * responds "Tell me more about your pet" if the
+		 * user mentions the word cat, dog, fish, or turtle
+		 * in their statement.
+		 *
+		 * Create addtional code (another else if) that
+		 * responds "He sounds like a pretty dank teacher"
+		 * if you mention "Robinette" in your statement */
+
+		else	
+		{
+			response = getRandomResponse();
+		}
+		return response;
+	}
+
+	/** Ex_02: The findKeyword() Method...
+	 * ========================================================= */
+	private int findKeyword(String statement, String goal, int startPos)
+	
+	{
+		String phrase = "";
+		phrase = statement.toLowerCase().trim();
+		
+		int psn = 0;
+		psn = phrase.indexOf(goal, startPos);
+		
+		if (psn >= 0)
+		{
+			String before = "";
+			String after = "";
+			
+			if (psn > 0)
+			{
+				before = phrase.substring(psn - 1, psn);
+			}
+			
+			if (psn + goal.length() < phrase.length())
+			{
+				after = phrase.substring(psn + goal.length(), psn + goal.length() + 1);
+			}
+			
+			if ((before.compareTo("a") < 0 || before.compareTo("z") > 0) && (after.compareTo("a") < 0 || after.compareTo("z") > 0))
+			{
+				return psn;
+			}
+			
+			else
+			{
+				return findKeyword(phrase, goal, psn + 1);
+			}
+		}
+		
+		return -1;
+		}
+					
+	
+	private int findKeyword(String statement, String goal)
+	
+	{
+		return findKeyword(statement, goal, 0);
+	}
+
+	/** getRandomResponse() method
+	 * =============================================================*/
+	/** Pick a default response to use if nothing else fits.
+	 * 	@return a non-committal string*/
+	 
+	private String getRandomResponse()
+	{
+		final int NUMBER_OF_RESPONSES = 4;
+		double r = Math.random();
+		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		String response = "";
+		
+		if (whichResponse == 0)
+			response = "Interesting, tell me more.";
+		else if (whichResponse == 1)
+			response = "Hmmm.";
+		else if (whichResponse == 2)
+			response = "Do you really think so?";
+		else if (whichResponse == 3)
+			response = "You don't say.";
+
+		return response;
+		
+	}
+}
---
+++ APCase_Magpie/Magpie2.java
@@ -0,0 +1,230 @@
+public class Magpie2
+
+{
+	/** Get a default greeting @return a greeting*/
+	public String getGreeting()
+	{
+		return "Hello... let's talk.";
+	}
+	
+	/** getResponse() method
+	 * ===========================================
+	 * 	Gives a response to a user statement
+	 *  @param statement (the user statement)
+	 * 	@return a response based on the rules given */
+	public String getResponse(String statement)
+	
+	{
+		String response = "";
+		
+		if (statement.length() == 0)			
+		{
+			System.out.println("Say something, please.");
+		}
+
+		/** Exercise_01:
+		 * ==================================================
+		 * 	Code that asks the user "Say something, please."
+		 * 	if you enter nothing, or if you accidentally hit
+		 * 	enter. Think to yourself: "What is the length of
+		 * 	an empty String?" */
+
+
+		/** To be completed in Exercise_02:
+		 * 	Modify the following code to use the findKeyword
+		 * 	Method (details in "Exercise_02" below. */
+		else if (findKeyword(statement, "No.") >= 0)			
+		{
+			response = "Why are you so negative?";
+		}
+	
+
+		else if (findKeyword(statement, "Mother") >= 0
+				|| findKeyword(statement, "Father") >= 0
+				|| findKeyword(statement, "Sister") >= 0
+				|| findKeyword(statement, "Brother") >= 0)
+		
+		{
+			response = "Tell me more about your family.";
+		}
+		
+		else if (findKeyword(statement, "Cat") >= 0
+				|| findKeyword(statement, "Dog") >= 0
+				|| findKeyword(statement, "Fish") >= 0
+				|| findKeyword(statement, "Horse") >= 0)
+		
+		{
+			response = "Tell me more about your pet.";	
+		}
+		
+		else if (findKeyword(statement, "Robinette") >= 0)			
+		{
+			response = "He sounds like a pretty wannabe dank teacher.";	
+		}
+		/** Exercise_03(Final)
+		 * ==================================================
+		 * Create additional code (another else if) that
+		 * responds "Tell me more about your pet" if the
+		 * user mentions the word cat, dog, fish, or turtle
+		 * in their statement.
+		 *
+		 * Create addtional code (another else if) that
+		 * responds "He sounds like a pretty dank teacher"
+		 * if you mention "Robinette" in your statement */
+
+	//else if (findKeyword(statement, "I want to", 0) >= 0)
+	//{
+	//	response = transformIWantToStatement(statement);
+	//}
+
+
+	else
+	{
+	// Look for a two word (you <something> me)
+	// pattern
+		int psn = findKeyword(statement, "you", 0);
+
+
+	if (psn >= 0
+			&& findKeyword(statement, "me", psn) >= 0)
+	{
+		response = transformYouMeStatement(statement);
+	}
+	else
+	{
+		response = getRandomResponse();
+	}
+	
+	}
+	return response;
+	}
+
+	/** Ex_02: The findKeyword() Method...
+	 * ========================================================= */
+	// private String transformIWantToStatement(String statement)
+	//{
+	  /**
+	   * trim the statement
+	   * variable lastChar = last character in statement
+	   * if lastChar is a period...
+	   *        remove the last character from statement
+	   *
+	   * Set new int psn to the result from...
+	   *        findKeyword() method @param statement, goal is "I want to "
+	   * Set new String restOfStatement to the rest of statement after the
+	   * "I want to ".
+	   * /
+	   * return "What would it mean to" + restOfStatement; **/
+	//}
+
+
+	/**
+	* Take a statement with "you <something> me" and transform it into
+	* "What makes you think that I <something> you?"
+	* @param statement the user statement, assumed to contain "you" followed by "me"
+	* @return the transformed statement
+	*/
+	private String transformYouMeStatement(String statement)
+	{
+	  /**
+	   * trim the statement
+	   * Set new String lastChar to the last character in statement
+	   * if lastChar is a period...
+	   *        remove the period
+	   *
+	   * Set new int psnOfYou to the result of findKeyword
+	   *        @param statement and "you"
+	   * Set new int psnOfMe to the result of findKeyword
+	   *      @param statement, "me", and psnOfYou + 3
+	   * Set new String restOfStatement to the rest of statement after "You" + 3,
+	   * and before "me".
+	   *
+	   * return "What makes you think that I " + restOfStatement + "you?"
+	   * */
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 2, statement.length() -1);
+		
+		if(lastChar.equals(".")){
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		 int psnOfYou = findKeyword(statement, "you ");
+		 int psnOfMe = findKeyword(statement, "me ", psnOfYou + 3);
+		 
+		 String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe);
+		 
+		 return "What makes you think that I " + restOfStatement + "you?";
+		
+		
+		
+	}
+	private int findKeyword(String statement, String goal, int startPos)
+	
+	{
+		String phrase = "";
+		phrase = statement.toLowerCase().trim();
+		
+		int psn = 0;
+		psn = phrase.indexOf(goal, startPos);
+		
+		if (psn >= 0)
+		{
+			String before = "";
+			String after = "";
+			
+			if (psn > 0)
+			{
+				before = phrase.substring(psn - 1, psn);
+			}
+			
+			if (psn + goal.length() < phrase.length())
+			{
+				after = phrase.substring(psn + goal.length(), psn + goal.length() + 1);
+			}
+			
+			if ((before.compareTo("a") < 0 || before.compareTo("z") > 0) && 
+			(after.compareTo("a") < 0 || after.compareTo("z") > 0))
+			{
+				return psn;
+			}
+			
+			else
+			{
+				return findKeyword(phrase, goal, psn + 1);
+			}
+		}
+		
+		return -1;
+		}
+					
+	
+	private int findKeyword(String statement, String goal)
+	
+	{
+		return findKeyword(statement, goal, 0);
+	}
+
+	/** getRandomResponse() method
+	 * =============================================================*/
+	/** Pick a default response to use if nothing else fits.
+	 * 	@return a non-committal string*/
+	 
+	private String getRandomResponse()
+	{
+		final int NUMBER_OF_RESPONSES = 4;
+		double r = Math.random();
+		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		String response = "";
+		
+		if (whichResponse == 0)
+			response = "Interesting, tell me more.";
+		else if (whichResponse == 1)
+			response = "Hmmm.";
+		else if (whichResponse == 2)
+			response = "Do you really think so?";
+		else if (whichResponse == 3)
+			response = "You don't say.";
+
+		return response;
+		
+	}
+}
